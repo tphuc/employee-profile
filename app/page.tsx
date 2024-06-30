e0a5a22424546903e815bd890d83323289ec52b8
FelixTran <felixtran2000@gmail.com>
2024-06-30 19:02:02 +0700

    init

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+
+import Link from "next/link";
+
+
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string }
+}) {
+
+  return <main className="relative flex min-h-screen flex-col max-w-[100vw] overflow-hidden  gap-4 p-4">
+   
+  </main>
+
+
+}
+
+
